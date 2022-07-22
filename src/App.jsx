05a6406fce45752e0abdd1ee3544d353bb391933
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-07-22 18:03:26 +0500

    It's Time To Push !

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [coins, setCoins] = useState([]);
+
+  const api = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
+  useEffect(() => {
+    axios.get(api).then((response) => {
+      setCoins(response.data)
+      console.log(response.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
-    <div className='App'>
+    <>
       <h1>
         Crypto Tracker App With React.JS!
       </h1>
-    </div>
+    </>
   );
 }
 
